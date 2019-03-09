842638d3c172033ef783be81faaa54b256225822
Adrian Darian <adarian@ucmerced.edu>
2019-03-08 21:09:45 -0800

    Lab4-2

--- Labs/Lab04-2/adarian.cpp
+++ Labs/Lab04-2/adarian.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-auto Partition(int* seq, int p, int r) {
+int Partition(int* seq, int p, int r) {
   int x = seq[r];
   int i = p - 1;
-  for (int j = p; j < r - 1; j++) {
+  for (int j = p; j <= r - 1; j++) {
     if (seq[j] <= x) {
       i += 1;
       swap(seq[i], seq[j]);
@@ -17,9 +17,15 @@ auto Partition(int* seq, int p, int r) {
   return i + 1;
 }
 
+int RandomizedPartition(int* seq, int p, int r) {
+  int i = (rand() % (r + 1 - p)) + p;
+  swap(seq[r], seq[i]);
+  return Partition(seq, p, r);
+}
+
 void QuickSort(int* seq, int p, int r) {
   if (p < r) {
-    auto q = Partition(seq, p, r);
+    auto q = RandomizedPartition(seq, p, r);
     QuickSort(seq, p, q - 1);
     QuickSort(seq, q + 1, r);
   }
@@ -38,7 +44,7 @@ int main(int argc, char **argv) {
   for(int i = 0; i < arraySize; i++)
     cin >> Sequence[i];
 
-  QuickSort(Sequence, 0, arraySize);
+  QuickSort(Sequence, 0, arraySize - 1);
 
   for (int c = 0; c < arraySize; c++) {
     cout << Sequence[c] << ';';
