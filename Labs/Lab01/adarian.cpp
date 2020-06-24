d289c532bfc4c870e213171a3a2c0e9d21ef2b6b
adriandarian <adarian@ucmerced.edu>
2020-06-24 16:27:07 -0700

    removed useless variable and added documentation

--- Labs/Lab01/adarian.cpp
+++ Labs/Lab01/adarian.cpp
@@ -4,37 +4,64 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-  int* Sequence;
-  int arraySize = 1;
+  int *Sequence;
+  auto arraySize = 1;
 
   // Get the size of the sequence
   cin >> arraySize;
   Sequence = new int[arraySize];
-    
-  // Read the sequence
-  for(int i=0; i<arraySize; i++)
-    cin >> Sequence[i];
 
-  // insertion sort as a lambda function
+  // Read the sequence
+  for (auto i = 0; i < arraySize; i++)
+  {
+    cin >> Sequence[i];
+  }
+
+  /*
+   * Insertion Sort as a lambda function
+   * 
+   * @params & sign allows function to take in all variables in the current parent scope
+   */ 
   auto result = [&]() {
-    int counter = 2;
-    for (int j = 1; j < arraySize; j++) {
-      int key = Sequence[j], i = j - 1;
-      while (i >= 0 && Sequence[i] > key) {
+    // Iterate through Sequence array starting from the 2nd index
+    for (auto j = 1; j < arraySize; j++)
+    {
+      /*
+       * Assign current element in array to key
+       * set the i as a placeholder for the previous element
+       */
+      auto key = Sequence[j], i = j - 1;
+
+      /*
+       * Check if the placeholder will be a value in the array
+       * Check if element at i is greater to the current place in the array
+       */
+      while (i >= 0 && Sequence[i] > key)
+      {
+        // Shift the previous element up by 1 place
         Sequence[i + 1] = Sequence[i];
+
+        // Decrease the placeholder by 1
         i--;
-      }  
+      }
+
+      // Assign element that was not sorted to it's correct place
       Sequence[i + 1] = key;
-      for (int x = 0; x < counter; x++) {
+
+      // Iterate from the beginning to the current place and print each value of array
+      for (auto x = 0; x <= j; x++)
+      {
         cout << Sequence[x] << ";";
       }
-      counter++;
+
       cout << endl;
     }
   };
 
+  // Call Lambda Function
   result();
 
   // Free allocated space
