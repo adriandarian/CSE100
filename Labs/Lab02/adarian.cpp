0aab653d78d8e5f1f9de52745ffc6ffc8ff2fce0
adriandarian <adarian@ucmerced.edu>
2020-06-28 01:56:49 -0700

    Dockerized Lab02

--- Labs/Lab02/adarian.cpp
+++ Labs/Lab02/adarian.cpp
@@ -4,18 +4,34 @@
 
 using namespace std;
   
+/*
+ * description: Recursive MergeSort
+ * 
+ * @params  seq   array of numbers
+ * @params  l     beginning of array
+ * @params  r     end of array
+ */
 void mergeSort(int* seq, int l, int r) { 
   if (l < r) { 
+    // Create middle placeholder
     int m = (l + r) / 2; 
+
     mergeSort(seq, l, m); 
     mergeSort(seq, m + 1, r); 
+
+    // Merge two numbers backwards as a lambda function
     auto merge = [&]() {
-      int i, j, k, n1 = m - l + 1, n2 =  r - m, L[n1], R[n2]; 
-      for (i = 0; i < n1; i++) 
+      auto i = 0, j = 0, k = 0, n1 = m - l + 1, n2 =  r - m;
+      int L[n1], R[n2]; 
+
+      for (i; i < n1; i++) 
         L[i] = seq[l + i]; 
-      for (j = 0; j < n2; j++) 
+      
+      for (j; j < n2; j++) 
         R[j] = seq[m + 1 + j]; 
+      
       i = 0, j = 0, k = l;
+
       while (i < n1 && j < n2) { 
         if (L[i] <= R[j]) { 
           seq[k] = L[i]; 
@@ -26,17 +42,21 @@ void mergeSort(int* seq, int l, int r) {
         } 
         k++; 
       } 
+
       while (i < n1) { 
         seq[k] = L[i]; 
         i++; 
         k++; 
       } 
+
       while (j < n2) { 
         seq[k] = R[j]; 
         j++; 
         k++; 
       } 
     };
+
+    // Call lambda function
     merge(); 
   } 
 } 
@@ -44,20 +64,20 @@ void mergeSort(int* seq, int l, int r) {
 int main(int argc, char **argv) {
 
   int* Sequence;
-  int arraySize = 1;
+  auto arraySize = 1;
 
   // Get the size of the sequence
   cin >> arraySize;
   Sequence = new int[arraySize];
     
   // Read the sequence
-  for(int i=0; i<arraySize; i++)
+  for(auto i = 0; i < arraySize; i++)
     cin >> Sequence[i];
 
-  mergeSort(Sequence, 0, arraySize-1);
+  mergeSort(Sequence, 0, arraySize - 1);
 
-  for (int c = 0; c < arraySize; c++) {
-    cout << Sequence[c] << ';';
+  for (auto i = 0; i < arraySize; i++) {
+    cout << Sequence[i] << ';';
   }
 
   // Free allocated space
