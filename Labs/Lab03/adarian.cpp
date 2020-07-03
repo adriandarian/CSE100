340bb628cd96d4d4a165c9c3f78c6fa782b6762f
adriandarian <adarian@ucmerced.edu>
2020-07-03 01:03:13 -0700

    Labo3 complete

--- Labs/Lab03/adarian.cpp
+++ Labs/Lab03/adarian.cpp
@@ -4,48 +4,61 @@
 
 using namespace std;
 
-int maxSubarray(int* seq, int low, int high) {
-  if (high == low) {
+int maxSubarray(int *seq, int low, int high)
+{
+  if (high == low)
+  {
     return seq[low]; // base case: only one element
-  } else {
-    int mid = (low + high) / 2;
-    int leftSum = maxSubarray(seq, low, mid);
-    int rightSum = maxSubarray(seq, mid + 1, high);
+  }
+  else
+  {
+    auto mid = (low + high) / 2;
+    auto leftSum = maxSubarray(seq, low, mid);
+    auto rightSum = maxSubarray(seq, mid + 1, high);
+
     auto crossSum = [&]() {
-      int leftSum = -MAX_INT, rightSum = -MAX_INT;
-      for (int i = mid, sum = 0; i >= low; i--) {
+      auto leftSum = -MAX_INT, rightSum = -MAX_INT;
+
+      for (auto i = mid, sum = 0; i >= low; i--)
+      {
         sum = sum + seq[i];
+
         if (sum > leftSum)
           leftSum = sum;
       }
-      for (int j = mid + 1, sum = 0; j <= high; j++) {
+
+      for (auto j = mid + 1, sum = 0; j <= high; j++)
+      {
         sum = sum + seq[j];
+
         if (sum > rightSum)
           rightSum = sum;
       }
+
       return leftSum + rightSum;
     };
-    
+
     if (leftSum >= rightSum and leftSum >= crossSum())
       return leftSum;
     else if (rightSum >= leftSum and rightSum >= crossSum())
       return rightSum;
-    else 
+    else
       return crossSum();
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-  int* Sequence;
-  int arraySize = 1;
+  int *Sequence;
+  auto arraySize = 1;
 
   // Get the size of the sequence
   cin >> arraySize;
   Sequence = new int[arraySize];
-    
+
   // Read the sequence
-  for(int i = 0; i < arraySize; i++)
+  for (auto i = 0; i < arraySize; i++)
     cin >> Sequence[i];
 
   cout << maxSubarray(Sequence, 0, arraySize - 1);
