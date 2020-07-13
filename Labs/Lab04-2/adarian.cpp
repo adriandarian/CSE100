615c9b64b8d0c4fd7e24fc90a1f5c44f412921ee
Adrian Darian <adarian@ucmerced.edu>
2020-07-12 21:54:57 -0700

    lab04-2 complete

--- Labs/Lab04-2/adarian.cpp
+++ Labs/Lab04-2/adarian.cpp
@@ -4,51 +4,94 @@
 
 using namespace std;
 
-int Partition(int* seq, int p, int r) {
-  int x = seq[r];
-  int i = p - 1;
-  for (int j = p; j <= r - 1; j++) {
-    if (seq[j] <= x) {
-      i += 1;
-      swap(seq[i], seq[j]);
-    }
-  }
-  swap(seq[i + 1], seq[r]);
-  return i + 1;
-}
+/**
+ * RandomizedPartition
+ * 
+ * @param seq array of elements to be sorted
+ * @param p left most
+ * @param r right most
+ * 
+ * @return new placement
+*/
+int RandomizedPartition(int *seq, int p, int r)
+{
+  auto i = (rand() % (r + 1 - p)) + p;
 
-int RandomizedPartition(int* seq, int p, int r) {
-  int i = (rand() % (r + 1 - p)) + p;
+  // swap value
   swap(seq[r], seq[i]);
+
+  /**
+   * HeapSort
+   * 
+   * @param seq array of elements to be sorted
+   * @param size length of array
+  */
+  auto Partition = [](int *seq, int p, int r) {
+    auto x = seq[r];
+    auto i = p - 1;
+
+    for (auto j = p; j <= r - 1; j++)
+    {
+      if (seq[j] <= x)
+      {
+        i++;
+
+        // swap value
+        swap(seq[i], seq[j]);
+      }
+    }
+
+    // swap value
+    swap(seq[i + 1], seq[r]);
+
+    return i + 1;
+  };
+
   return Partition(seq, p, r);
 }
 
-void QuickSort(int* seq, int p, int r) {
-  if (p < r) {
+/**
+ * QuickSort
+ * 
+ * @param seq array of elements to be sorted
+ * @param p left most
+ * @param r right most
+*/
+void QuickSort(int *seq, int p, int r)
+{
+  if (p < r)
+  {
+    // get random mid
     auto q = RandomizedPartition(seq, p, r);
+
+    // Recusive QuickSort on left half
     QuickSort(seq, p, q - 1);
+
+    // Recursive QuickSort on right half
     QuickSort(seq, q + 1, r);
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-  int* Sequence;
-  int arraySize = 1;
+  int *Sequence;
+  auto arraySize = 1;
 
   // Get the size of the sequence
   cin >> arraySize;
   Sequence = new int[arraySize];
-    
+
   // Read the sequence
-  for(int i = 0; i < arraySize; i++)
+  for (auto i = 0; i < arraySize; i++)
     cin >> Sequence[i];
 
+  // Runs QuickSort algoritm on sequence
   QuickSort(Sequence, 0, arraySize - 1);
 
-  for (int c = 0; c < arraySize; c++) {
+  // Print out each element in sorted array
+  for (auto c = 0; c < arraySize; c++)
     cout << Sequence[c] << ';';
-  }
 
   // Free allocated space
   delete[] Sequence;
