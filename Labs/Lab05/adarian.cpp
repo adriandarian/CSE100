c7d5cfce3cf05c4b679174fc010eca1e4646d2aa
Adrian Darian <adarian@ucmerced.edu>
2020-07-17 05:40:51 -0700

    working and documented lab05

--- Labs/Lab05/adarian.cpp
+++ Labs/Lab05/adarian.cpp
@@ -5,67 +5,107 @@
 
 using namespace std;
 
-vector<vector<int>> CountingSort(vector<vector<int>> container, vector<int> vec) {
-  vector<int> Count(length,0);
-  vector<vector<int>> temp;
-  temp.resize(vec.size());
-  
-  for (int i = 0; i < vec.size(); i++) {
-    Count[vec[i]]++;
-  }
+/**
+ * RadixSort
+ * 
+ * @param container 2D list of numbers
+*/
+void RadixSort(vector<vector<int>> container)
+{
+  /**
+   * CountingSort
+   * 
+   * @param container 2D list of numbers
+   * @param vec sub list from the main container
+   * 
+   * @return 2D list of numbers
+  */
+  auto CountingSort = [&](vector<vector<int>> container, vector<int> vec) {
+    vector<int> Count(length, 0);
+    vector<vector<int>> temp;
+    temp.resize(vec.size());
 
-  for (int i = 1; i < Count.size(); i++) {
-    Count[i] += Count[i - 1];
-  }
+    for (auto i = 0; i < vec.size(); i++)
+    {
+      Count[vec[i]]++;
+    }
 
-  for (int i = 0; i < vec.size(); i++) {
-    temp[i].resize(length);
-  }
+    for (auto i = 1; i < Count.size(); i++)
+    {
+      Count[i] += Count[i - 1];
+    }
 
-  for (int i = vec.size()-1; i >= 0; i--) {
-    swap(temp[Count[vec[i]] - 1], container[i]);
-    Count[vec[i]] -= 1;
-  }
+    for (auto i = 0; i < vec.size(); i++)
+    {
+      temp[i].resize(length);
+    }
 
-  return temp;
-}
+    for (int i = vec.size() - 1; i >= 0; i--)
+    {
+      swap(temp[Count[vec[i]] - 1], container[i]);
+      Count[vec[i]] -= 1;
+    }
 
-void RadixSort(vector<vector<int>> container) {
-  for (int i = length - 1; i >= 0; i--) {
+    return temp;
+  };
+
+  // Algorithm for RadixSort
+  for (auto i = length - 1; i >= 0; i--)
+  {
     vector<int> vec;
-    for (int j = 0; j < container.size(); j++ )
+
+    for (auto j = 0; j < container.size(); j++)
+    {
       vec.push_back(container[j][i]);
+    }
+
     container = CountingSort(container, vec);
   }
-  
-  for(int i = 0; i < container.size(); i++){
-    for (int j = 0; j < container[i].size(); j++){
-      cout << container[i][j] << ";";
+
+  /**
+   * Print
+   * 
+   * @param container 2D list of numbers
+   * 
+   * @return nothing because it will call a print lambda function to print to the console the sorted list
+  */
+  auto print = [&]() {
+    for (auto i = 0; i < container.size(); i++)
+    {
+      for (auto j = 0; j < container[i].size(); j++)
+      {
+        cout << container[i][j] << ";";
+      }
+
+      cout << endl;
     }
-    cout << endl;
-  }
+  };
+
+  print();
 }
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[])
+{
   vector<vector<int>> container;
   int val;
-  int vecSize = 0;
+  auto vecSize = 0;
 
   // Get the size of each sequence
   cin >> vecSize;
   container.resize(vecSize);
-  
+
   // Read the sequences
-  for(int i = 0; i < vecSize; i++) {
-    for(int j = 0; j < length; j++) {
+  for (auto i = 0; i < vecSize; i++)
+  {
+    for (auto j = 0; j < length; j++)
+    {
       cin >> val;
       container[i].push_back(val);
     }
   }
 
+  // Performs RadixSort on 2D list of numbers
   RadixSort(container);
 
   return 0;
 }
-
-
