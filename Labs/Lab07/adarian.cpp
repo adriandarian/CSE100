fe47445f11eaf395bc434959f99ce585b19ffa6e
Adrian Darian <adarian@ucmerced.edu>
2020-07-22 21:23:25 -0700

    complete lab07

--- Labs/Lab07/adarian.cpp
+++ Labs/Lab07/adarian.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // Get the size of the sequence
   auto n = 1;
   int *p;
@@ -13,21 +14,25 @@ int main(int argc, char **argv) {
   p = new int[n + 1];
   p[0] = 0;
 
-  for (auto i = 1; i <= n; i++) {
+  for (auto i = 1; i <= n; i++)
+  {
     cin >> p[i];
   }
 
-  auto output = [&](){ // Extended Bottom Up Cut Rod
+  auto output = [&]() { // Extended Bottom Up Cut Rod
     auto *r = new int[n + 1];
     auto *s = new int[n + 1];
     int q;
 
     r[0] = 0;
 
-    for (auto i = 1; i <= n; i++) {
+    for (auto i = 1; i <= n; i++)
+    {
       q = INT_MIN;
-      for (int j = 1; j <= i; j++) {
-        if (q < p[j] + r[i - j]) {
+      for (int j = 1; j <= i; j++)
+      {
+        if (q < p[j] + r[i - j])
+        {
           q = p[j] + r[i - j];
           s[i] = j;
         }
@@ -37,7 +42,8 @@ int main(int argc, char **argv) {
 
     auto result = [&]() { // Print Cut Rod Solution
       cout << r[n] << endl;
-      while (n > 0) {
+      while (n > 0)
+      {
         cout << s[n] << " ";
         n -= s[n];
       }
